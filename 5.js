e654a83789334c462e4beb597a28ca77cfa21e28
DeveloperAmrit <kittyamrit005@gmail.com>
2024-06-21 21:41:15 +0530

    Initial commit

---
+++ 5.js
@@ -0,0 +1,17 @@
+let subpages = document.getElementsByClassName('subpage')
+let subpage = document.getElementById('subpage1')
+let color_of_subpages_list = ['gray','pink','yellow','lightgreen']
+
+for(let i=0;i<subpages.length;i++){
+    subpages[i].style.zIndex = i+1
+    subpages[i].style.background = color_of_subpages_list[i]
+    console.log('WORKING')
+}
+
+// REMOVE THESE
+
+let textsamples = document.getElementsByClassName('textsample')
+
+for(let i=0;i<textsamples.length;i++){
+    textsamples[i].innerText = `SubPage-${i+1}`
+}
